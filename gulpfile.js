982b9c7711644f18c544804ffaa3df288735ce58
Karl Patrick Espirituq <wiwa.espiritu@gmail.com>
2017-07-05 17:23:04 +0800

    modifications

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var pkg = require('./package.json');
 
 // Set the banner content
 var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Emu Run - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
     ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
     ' */\n',
